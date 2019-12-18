8e86136664df34e695a86365e2ae7890c81c5391
Brian Menini <bmenini@hotmail.com>
2019-12-17 21:42:15 -0600

    Display result to page under buttons

--- functions.js
+++ functions.js
@@ -9,26 +9,32 @@ var randomMove = textArray[Math.floor(Math.random() * textArray.length)];
 return randomMove;
 }
 
+
+
  function playRound(playerSelection, computerSelection) {
     
-    if (playerSelection == "Rock" && computerSelection == "Rock" || 
+    if (
+        playerSelection == "Rock" && computerSelection == "Rock" || 
         playerSelection == "Paper" && computerSelection == "Paper" ||
-        playerSelection == "Scissors" && computerSelection == "Scissors"){
-        
-
-            return message="It was a tie, both selected " + playerSelection + ".";
-
-        } else if 
-        (playerSelection == "Rock" && computerSelection === "Scissors" || 
+        playerSelection == "Scissors" && computerSelection == "Scissors")
+        {
+            var message = document.getElementById('display').innerHTML = "It was a tie, both selected " + playerSelection + ".";
+            return message;
+            //return message="It was a tie, both selected " + playerSelection + ".";
+        } else if (
+        playerSelection == "Rock" && computerSelection == "Scissors" || 
         playerSelection == "Scissors" && computerSelection == "Paper" ||
         playerSelection == "Paper" && computerSelection == "Rock") {
-        
-            return message="Player Wins, " + playerSelection + " beats " + computerSelection;
+            var message = document.getElementById('display').innerHTML = "Player Wins, " + playerSelection + " beats " + computerSelection;
+            return message;
+            //return message="Player Wins, " + playerSelection + " beats " + computerSelection;
         }
+        else {
+            var message = document.getElementById('display').innerHTML = "Player Wins, " + playerSelection + " beats " + computerSelection;
+            return message;
+            //return message="Computer Wins, " + computerSelection + " beats " + playerSelection ;}
     }
-    function playerSelection(item) {
-        var move = item;
-        return move;
-    }
+}
+
   
        
